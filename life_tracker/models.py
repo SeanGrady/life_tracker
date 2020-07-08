b78dae6ca8d55147bb28a7872ceaa73925cdda8e
Sean <vedicmonk@gmail.com>
2020-07-08 14:04:03 -0700

    Added AppUser table and relationships to existing models

--- life_tracker/models.py
+++ life_tracker/models.py
@@ -5,8 +5,10 @@ from sqlalchemy import (
     Date,
     Float,
     DateTime,
+    ForeignKey,
 )
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
@@ -36,13 +38,38 @@ class WeighIn(Base):
     id = Column(Integer, primary_key=True)
     date = Column(Date)
     weight_lbs = Column(Float)
+    app_user_id = Column(Integer, ForeignKey('app_user.id'))
+    app_user = relationship(
+        'AppUser',
+        back_populates='weigh_ins',
+    )
 
 
 class MoodSurveyResponse(Base):
     __tablename__ = 'mood_survey_response'
     id = Column(Integer, primary_key=True)
+    app_user_id = Column(Integer, ForeignKey('app_user.id'))
+    app_user = relationship(
+        'AppUser',
+        back_populates='mood_survey_responses',
+    )
     mood = Column(Integer)
     energy = Column(Integer)
     adderall_crash = Column(Integer)
     sleep_hours = Column(Integer)
     date_time = Column(DateTime)
+
+class AppUser(Base):
+    __tablename__ = 'app_user'
+    id = Column(Integer, primary_key=True)
+    mood_survey_responses = relationship(
+        'MoodSurveyResponse',
+        back_populates='app_user',
+    )
+    weigh_ins = relationship(
+        'WeighIn',
+        back_populates='app_user',
+    )
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
