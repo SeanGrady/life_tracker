c8ad565e79e2a93030c68e8be80b21776d2dd015
Sean <vedicmonk@gmail.com>
2020-07-08 14:03:36 -0700

    Changed quote types and fixed spelling errors in models.py

--- life_tracker/models.py
+++ life_tracker/models.py
@@ -13,7 +13,7 @@ Base = declarative_base()
 
 
 class LoseitFood(Base):
-    __tablename__ = "loseit_food"
+    __tablename__ = 'loseit_food'
     id = Column(Integer, primary_key=True)
     raw_date = Column(Date)
     raw_name = Column(String)
@@ -32,17 +32,17 @@ class LoseitFood(Base):
 
 
 class WeighIn(Base):
-    __tablename__ = "weigh_in"
+    __tablename__ = 'weigh_in'
     id = Column(Integer, primary_key=True)
     date = Column(Date)
     weight_lbs = Column(Float)
 
 
 class MoodSurveyResponse(Base):
-    __tablename__ = "mood_survey_response"
+    __tablename__ = 'mood_survey_response'
     id = Column(Integer, primary_key=True)
     mood = Column(Integer)
     energy = Column(Integer)
-    adderal_crash = Column(Integer)
+    adderall_crash = Column(Integer)
     sleep_hours = Column(Integer)
     date_time = Column(DateTime)
