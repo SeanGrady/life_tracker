3bda1cb349073a1455325c668073558b96494229
Sean <vedicmonk@gmail.com>
2020-07-14 18:58:27 -0700

    Added sleep survey table

--- life_tracker/models.py
+++ life_tracker/models.py
@@ -59,6 +59,28 @@ class WeighIn(Base):
     )
 
 
+class SleepSurveyResponse(Base):
+    __tablename_ = 'sleep_survey_response'
+    app_user_id = Column(
+        Integer,
+        ForeignKey('app_user.id'),
+        nullable=False,
+        primary_key=True,
+    )
+    app_user = relationship(
+        'AppUser',
+        back_populates='sleep_survey_responses',
+    )
+    date_time = Column(
+        DateTime,
+        primary_key=True,
+    )
+    sleep_quality = Column(Integer)
+    sleep_hours = Column(Float)
+    rise_ease = Column(Integer)
+
+
+
 class MoodSurveyResponse(Base):
     __tablename__ = 'mood_survey_response'
     app_user_id = Column(
@@ -85,6 +107,10 @@ class AppUser(Base):
     id = Column(Integer, primary_key=True)
     mood_survey_responses = relationship(
         'MoodSurveyResponse',
+        back_populates='app_user',
+    )
+    sleep_survey_responses = relationship(
+        'SleepSurveyResponse',
         back_populates='app_user',
     )
     weigh_ins = relationship(
