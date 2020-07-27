af58ed2dbb3be59c9cf0218cc9ab05acbd00ad5c
Sean <vedicmonk@gmail.com>
2020-07-26 17:52:36 -0700

    Refactored models to use mixins for boilerplate code

--- life_tracker/models.py
+++ life_tracker/models.py
@@ -8,25 +8,56 @@ from sqlalchemy import (
     ForeignKey,
     UniqueConstraint,
 )
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import (
+    declarative_base,
+    declared_attr,
+)
+from sqlalchemy.orm import (
+    relationship,
+)
 
 
 Base = declarative_base()
 
 
-class LoseitFood(Base):
+class AppUserMixin(object):
+    @declared_attr
+    def app_user_id(cls):
+        return Column(
+            Integer,
+            ForeignKey('app_user.id'),
+            nullable=False,
+            primary_key=True,
+        )
+
+
+class AppUser(Base):
+    __tablename__ = 'app_user'
+    id = Column(Integer, primary_key=True)
+    mood_survey_responses = relationship(
+        'MoodSurveyResponse',
+        backref='app_user',
+    )
+    sleep_survey_responses = relationship(
+        'SleepSurveyResponse',
+        backref='app_user',
+    )
+    weigh_ins = relationship(
+        'WeighIn',
+        backref='app_user',
+    )
+    loseit_foods = relationship(
+        'LoseitFood',
+        backref='app_user',
+    )
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+
+
+class LoseitFood(Base, AppUserMixin):
     __tablename__ = 'loseit_food'
     id = Column(Integer, primary_key=True)
-    app_user_id = Column(
-        Integer,
-        ForeignKey('app_user.id'),
-        nullable=False,
-    )
-    app_user = relationship(
-        'AppUser',
-        back_populates='loseit_foods',
-    )
     raw_date = Column(Date)
     raw_name = Column(String)
     raw_type = Column(String)
@@ -43,84 +74,32 @@ class LoseitFood(Base):
     raw_sodium_mg = Column(Float)
 
 
-class WeighIn(Base):
+class WeighIn(Base, AppUserMixin):
     __tablename__ = 'weigh_in'
     id = Column(Integer, primary_key=True)
     date = Column(Date)
     weight_lbs = Column(Float)
-    app_user_id = Column(
-        Integer,
-        ForeignKey('app_user.id'),
-        nullable=False,
-    )
-    app_user = relationship(
-        'AppUser',
-        back_populates='weigh_ins',
-    )
 
 
-class SleepSurveyResponse(Base):
+class GformResponseMixin(object):
+    @declared_attr
+    def date_time(cls):
+        Column(
+            DateTime,
+            primary_key=True,
+        )
+
+
+class SleepSurveyResponse(Base, AppUserMixin, GformResponseMixin):
     __tablename__ = 'sleep_survey_response'
-    app_user_id = Column(
-        Integer,
-        ForeignKey('app_user.id'),
-        nullable=False,
-        primary_key=True,
-    )
-    app_user = relationship(
-        'AppUser',
-        back_populates='sleep_survey_responses',
-    )
-    date_time = Column(
-        DateTime,
-        primary_key=True,
-    )
     sleep_quality = Column(Integer)
     sleep_hours = Column(Float)
     rise_ease = Column(Integer)
 
 
-
-class MoodSurveyResponse(Base):
+class MoodSurveyResponse(Base, AppUserMixin, GformResponseMixin):
     __tablename__ = 'mood_survey_response'
-    app_user_id = Column(
-        Integer,
-        ForeignKey('app_user.id'),
-        nullable=False,
-        primary_key=True,
-    )
-    app_user = relationship(
-        'AppUser',
-        back_populates='mood_survey_responses',
-    )
     mood = Column(Integer)
     energy = Column(Integer)
     adderall_crash = Column(Integer)
     sleep_hours = Column(Float)
-    date_time = Column(
-        DateTime,
-        primary_key=True,
-    )
-
-class AppUser(Base):
-    __tablename__ = 'app_user'
-    id = Column(Integer, primary_key=True)
-    mood_survey_responses = relationship(
-        'MoodSurveyResponse',
-        back_populates='app_user',
-    )
-    sleep_survey_responses = relationship(
-        'SleepSurveyResponse',
-        back_populates='app_user',
-    )
-    weigh_ins = relationship(
-        'WeighIn',
-        back_populates='app_user',
-    )
-    loseit_foods = relationship(
-        'LoseitFood',
-        back_populates='app_user',
-    )
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String)
