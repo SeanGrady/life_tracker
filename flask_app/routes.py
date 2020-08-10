b9b6a0f87890b715e42fc4a40934dd1f747f1918
Sean <vedicmonk@gmail.com>
2020-08-10 14:47:42 -0700

    Added simple template functionality to index view, as well as accompanying templates

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,7 +1,18 @@
 from flask_app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "It's a flask app!"
+    user = {
+        'username': 'Sean',
+    }
+    title = 'LifeTracker'
+
+    template_vars = {
+        'user':user,
+        'title':title,
+    }
+
+    return render_template('index.html', **template_vars)
