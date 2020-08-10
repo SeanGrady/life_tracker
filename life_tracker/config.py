d5dbc3ebf1f73b6a653ec475b7b0f742d6ec662a
Sean <vedicmonk@gmail.com>
2020-08-10 15:33:06 -0700

    Added secret key variable to config along with default value for testing

--- life_tracker/config.py
+++ life_tracker/config.py
@@ -6,4 +6,8 @@ class AppConfig(object):
         'DATABASE_URI',
         'postgres+psycopg2://postgres:password@127.0.0.1:5432/lifetracker-test'
     )
+    SECRET_KEY = os.getenv(
+        'SECRET_KEY',
+        'no-peeking'
+    )
 
