a19de26654f42a7379c3fe3abb0aa10b7a12cc57
Sean <vedicmonk@gmail.com>
2020-08-10 17:35:18 -0700

    Added shell context function using crud.py's contextual session

--- life_tracker/lifetracker.py
+++ life_tracker/lifetracker.py
@@ -3,3 +3,13 @@ from backend.crud import contextual_session
 from backend.models import (
     AppUser,
 )
+
+
+@app.shell_context_processor
+def make_shell_context():
+    with contextual_session() as session:
+        context = {
+            'session': session,
+            'AppUser': AppUser,
+        }
+        return context
