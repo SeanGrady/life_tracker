f0325bfed478f308733ccc47fd5218bf04054f8d
Sean <vedicmonk@gmail.com>
2020-08-21 14:12:25 -0700

    refactored cronometer wrapper class to be export-agnostic, load data with pandas, and then insert into the database based onexport column names

--- life_tracker/backend/cronometer.py
+++ life_tracker/backend/cronometer.py
@@ -1,5 +1,12 @@
 import pandas as pd
 from .crud import contextual_session
+from .models import (
+    CronometerNote,
+    CronometerDailySummary,
+    CronometerExercise,
+    CronometerServing,
+    CronometerBiometric,
+)
 
 
 def snake_case_cronometer_column_name(column_name):
@@ -27,11 +34,53 @@ def snake_case_cronometer_column_name(column_name):
     return column_name
 
 class CronometerDataLoader(object):
-    def __init__(self, user, export_directory):
+    def __init__(self, user, session, export_directory):
         self.user = user
+        self.session = session
         self.export_directory = export_directory
-        self.exercise_export_filename = 'exercises.csv'
+        self.export_types = [
+            'exercise',
+            'biometric',
+            'serving',
+            'note',
+            'daily_summary',
+        ]
+        self.export_models = {
+            'exercise': CronometerExercise,
+            'biometric': CronometerBiometric,
+            'serving': CronometerServing,
+            'note': CronometerNote,
+            'daily_summary': CronometerDailySummary,
+        }
+        self.export_filenames = {
+            'exercise': 'exercises.csv',
+            'biometric': 'biometrics.csv',
+            'serving': 'servings.csv',
+            'note': 'notes.csv',
+            'daily_summary': 'dailySummary.csv',
+        }
 
-    def load_exercise_data(self):
-        exercise_csv = self.export_directory / self.exercise_export_filename
-        exercise_data = pd.load_csv(exercise_csv)
+
+    def load_exports_into_database(self):
+        for export in export_types:
+            export_filepath = self.export_directory / self.export_filenames[export]
+            export_data = pd.load_csv(export_filepath)
+            export_data['app_user_id'] = self.user.id
+
+            """Rename the columns so that they match the model/table's column names"""
+            column_names = export_data.columns.values.tolist()
+            column_rename_map = {
+                name: snake_case_cronometer_column_name(name)
+                for name in column_names
+            }
+            export_data.rename(columns=column_rename_map)
+
+            import pdb;pdb.set_trace()
+            export_data.to_sql(
+                name=self.export_models[export].__table__.name,
+                con=self.session.get_bind(),
+                if_exists='append',
+            )
+
+
+
